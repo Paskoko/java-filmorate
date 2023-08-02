5d6cbd3e2b2cf5ab0fe8d79ed3b54b391df1d22d
Paskoko <paskoko@yandex.ru>
2023-08-03 01:43:00 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Positive;
 import java.time.LocalDate;
@@ -14,7 +13,6 @@ import java.time.LocalDate;
 @Data
 public class Film {
     private int ID;
-    @NotNull
     @NotBlank
     private String name;
     private String description;
